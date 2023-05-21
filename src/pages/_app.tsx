330a54b0f454b397c525db127c7b632571064bbe
erickdevup <erickdevup@gmail.com>
2023-05-21 10:27:40 +0200

    analitycs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,18 @@ import Script from "next/script";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-YFREEK5Y84"
+      ></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-YFREEK5Y84');`}
+      </Script>
       <Component {...pageProps} />
     </>
   );
